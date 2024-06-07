e19176ebd301cffe62aced8a489bdc230d2d265e
schluis <luis.scheuch@posteo.de>
2024-06-07 20:11:53 +0200

    add zmp

--- crates/control/src/motion/walking_engine.rs
+++ crates/control/src/motion/walking_engine.rs
@@ -90,6 +90,7 @@ impl WalkingEngine {
                 .parameters
                 .swinging_arms
                 .torso_tilt_compensation_factor,
+            *cycle_context.zero_moment_point,
         );
 
         let arm_compensation = compensate_arm_motion_with_torso_tilt(
@@ -190,7 +191,11 @@ impl WalkingEngine {
         })
     }
 
-    fn torso_adjustment(&self, torso_tilt_compensation_factor: f32) -> f32 {
+    fn torso_adjustment(
+        &self,
+        torso_tilt_compensation_factor: f32,
+        zero_moment_point: Point2<Ground>,
+    ) -> f32 {
         torso_tilt_compensation_factor // * self.engine.
     }
 }
